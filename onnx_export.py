ecd5fcbce52086acd6c76a5d72f5fb1f67cc5e60
Judy_with_old_machine <w00zch@163.com>
2025-02-17 09:43:21 +0800

    update onnx

--- onnx_export.py
+++ onnx_export.py
@@ -21,18 +21,41 @@ def main():
     parser.add_argument("--export_path", type=str)
     parser.add_argument("--resolution", type=int, nargs="+", default=224)
     parser.add_argument("--op_set", type=int, default=16)
-    parser.add_argument("--bs", type=int, default=2)
+    parser.add_argument("--bs", type=int, default=1)
+    parser.add_argument("-wp", "--weight_path", default="pretrained_weights/DRIVE/checkpoint-epoch40.pth", type=str,
+                        help='the path of wetght.pt')
 
     args = parser.parse_args()
 
     resolution = (960, 999) # 999x960
+    #checkpoint = torch.load(args.weight_path)
+    
     
     yaml = YAML(typ='safe', pure=True)
     with open('config_quan.yaml', 'r') as file:
         CFG = Bunch(yaml.load(file))
+        
     model = get_instance(models, 'model', CFG)
+    #model.load_state_dict(checkpoint['state_dict'])
+    
+    
+    
+    
+    # 加载预训练模型的状态字典
+    pretrained_dict = torch.load(args.weight_path)
 
-    dummy_input = torch.rand(1, 1, 1008, 1008) #torch.rand((args.bs, 1, *resolution))
+    # 移除因为分布式训练而有的 'module.' 前缀
+    new_state_dict = {}
+    for k, v in pretrained_dict.items():
+        if k.startswith('module.'):
+            k = k[7:]  # 移除 'module.' 前缀
+        new_state_dict[k] = v
+
+    # 加载修改后的状态字典到目标模型
+    model.load_state_dict(new_state_dict, strict=False)
+    
+
+    dummy_input = torch.rand(1, 1, 592, 592) #torch.rand((args.bs, 1, *resolution))
     export_onnx(model, args.export_path, dummy_input, simplify=True, opset=args.op_set)
 
 
